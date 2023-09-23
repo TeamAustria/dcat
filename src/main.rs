70962035cf4602a6ad8e0a38cf48ef873a085bd9
Fabian <fabian.paul.gurtner@gmail.com>
2023-09-23 09:00:37 +0200

    Add command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+use std::fs::File;
 use std::io::{Read, stdin, stdout, Write};
 use std::thread;
 use std::time::Duration;
+
+use clap::Parser;
 use delay_queue::{Delay, DelayQueue};
 
 fn main() {
+    let args = Args::parse();
+
     let mut queue: DelayQueue<Delay<Vec<u8>>> = DelayQueue::new();
 
     let mut consumer_queue = queue.clone();
@@ -20,27 +25,52 @@ fn main() {
         }
     });
 
-    let mut reader = stdin();
-    let mut buffer = [0; 1024];
+    let mut readers: Vec<Box<dyn Read>> = args.input_files
+        .unwrap_or(vec!["-".to_string()])
+        .iter()
+        .map(|file| match file.as_str() {
+            "-" => Box::new(stdin()) as Box<dyn Read>,
+            _ => Box::new(File::open(file).expect("Failed to open file")),
+        })
+        .collect();
 
-    loop {
-        let read = reader.read(&mut buffer).unwrap();
-        queue.push(Delay::for_duration(
-            buffer[0..read].to_vec(),
-            Duration::from_secs(1),
-        ));
-        if read == 0 {
-            break;
+    let mut buffer = vec![0u8; args.buffer_size as usize];
+
+    for reader in readers.iter_mut() {
+        loop {
+            let read = reader.read(&mut buffer).unwrap();
+            if read == 0 {
+                break;
+            }
+            queue.push(Delay::for_duration(
+                buffer[0..read].to_vec(),
+                Duration::from_millis(args.delay_ms),
+            ));
         }
     }
+    queue.push(Delay::for_duration(
+        vec![],
+        Duration::from_millis(args.delay_ms)
+    ));
 
     consumer_handle.join().unwrap();
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value_t = 1000)]
+    delay_ms: u64,
+    #[arg(short, long, default_value_t = 1024 * 1024)]
+    buffer_size: u64,
+    input_files: Option<Vec<String>>,
 }
 
 #[cfg(test)]
 mod tests {
     use std::io::Write;
     use std::process::Stdio;
+
     use rand::RngCore;
 
     fn generate_test_data() -> Vec<u8> {
